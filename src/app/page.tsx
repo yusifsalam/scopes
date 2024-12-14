dfb496bcbf55d30e9e2b53847ae5ca7f95ce27c4
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2024-12-14 21:05:00 +0200

    simple static page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
+import { getScopes } from "./getScopes";
+
 export default async function IndexPage() {
-  const data = await fetch("http://localhost:3000/api/scopes", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const scopes = await data.json();
-  console.log("data", scopes);
+  const scopes = await getScopes();
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       <p>Hello world</p>
