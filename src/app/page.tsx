f7f6348687644ddd8a5ed856b35ed4eb440d4b2e
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2024-12-18 22:22:04 +0200

    eslint warn for any instead of error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,36 @@ import { createClient } from "@/supabase/server";
 export default async function IndexPage() {
   const supabase = await createClient();
   const { data: scopes } = await supabase
-      .from('horoscopes')
-      .select(`
+    .from("horoscopes")
+    .select(
+      `
         id,
         scope,
         date,
         signs (
           name
         )
-      `)
-      .eq('date', new Date().toISOString().split('T')[0])
-      .order('sign_id')
-  console.log(scopes)
+      `,
+    )
+    .eq("date", new Date().toISOString().split("T")[0])
+    .order("sign_id");
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       <p>Hello world</p>
-      {scopes?.map(s => <SingleScope key={s.id} scope={s as any} />)}
+      {scopes?.map((s) => <SingleScope key={s.id} scope={s as any} />)}
     </div>
   );
 }
 
-const SingleScope = ({scope}: {scope: {id: number, scope: string, date: Date, signs: {name: string}} }) => {
+const SingleScope = ({
+  scope,
+}: {
+  scope: { id: number; scope: string; date: Date; signs: { name: string } };
+}) => {
   return (
     <div>
-      <h2>{ scope.signs.name}</h2>
-      <p>{ scope.scope} </p>
+      <h2>{scope.signs.name}</h2>
+      <p>{scope.scope} </p>
     </div>
-  )
-}
+  );
+};
