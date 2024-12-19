ba040aee8d0f193f37655e65766092b10e55dcde
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2024-12-19 20:21:20 +0200

    prevent search engine indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="robots" content="noindex" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
