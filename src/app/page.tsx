a488386b2480fd1ae1793e91e15b395fa9b03af4
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2024-12-24 10:32:59 +0200

    tweak main heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,15 @@ export default async function IndexPage() {
   );
   return (
     <div className="prose stretch mx-auto flex w-full max-w-md flex-col px-4 py-8">
-      <h1>Rektor &apos;scopes for {new Date().toDateString()}</h1>
+      <h1 className="text-center">
+        Rektor &apos;scopes
+        <br />
+        {new Date().toLocaleDateString("en-US", {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+        })}
+      </h1>
       <Scopes data={data} />
     </div>
   );
