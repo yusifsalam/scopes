26c6c92a03e0bdc154a3c469ab6c457e13084930
Yusif Salam-zade <yusif.salam-zade@reaktor.com>
2025-01-18 19:37:09 +0200

    set up static rewrites

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,33 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      { source: "/kauris", destination: "/capricorn" },
+      { source: "/vesimies", destination: "/aquarius" },
+      { source: "/kalat", destination: "/pisces" },
+      { source: "/oinas", destination: "/aries" },
+      { source: "/härkä", destination: "/taurus" },
+      { source: "/kaksoset", destination: "/gemini" },
+      { source: "/rapu", destination: "/cancer" },
+      { source: "/leijona", destination: "/leo" },
+      { source: "/neitsyt", destination: "/virgo" },
+      { source: "/vaaka", destination: "/libra" },
+      { source: "/skorpioni", destination: "/scorpio" },
+      { source: "/jousimies", destination: "/sagittarius" },
+
+      { source: "/ram", destination: "/aries" },
+      { source: "/bull", destination: "/taurus" },
+      { source: "/twin", destination: "/gemini" },
+      { source: "/crab", destination: "/cancer" },
+      { source: "/lion", destination: "/leo" },
+      { source: "/virgin", destination: "/virgo" },
+      { source: "/horizontal", destination: "/libra" },
+      { source: "/scorpion", destination: "/scorpio" },
+      { source: "/centaur", destination: "/sagittarius" },
+      { source: "/pisces%20fishes", destination: "/pisces" },
+    ];
+  },
 };
 
 export default nextConfig;
