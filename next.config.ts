d45ccaca6c217f6509de43b5d54b0a68188c9e5e
Yusif Salam-zade <yusif.salam-zade@reaktor.com>
2025-01-19 09:37:06 +0200

    set image min cache ttl

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    minimumCacheTTL: 60 * 60 * 24, // 1 day
     remotePatterns: [
       {
         protocol: "https",
