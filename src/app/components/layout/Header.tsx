5637459d468b3ed3cb1ba2d203eaf9993e5d1ab9
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2025-04-26 15:08:43 +0300

    tweak header design

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -6,19 +6,26 @@ type HeaderProps = {
 
 const Header = ({ sign }: HeaderProps) => {
   return (
-    <div className="flex items-center justify-between">
-      <h1>
-        Rektor &apos;scopes
-        <br />
-        {new Date().toLocaleDateString("en-US", {
-          month: "short",
-          day: "numeric",
-          year: "numeric",
-        })}
-        {sign && <span className="capitalize">: {sign}</span>}
-      </h1>
+    <header className="flex items-center justify-between py-6">
+      <div>
+        <h1 className="text-3xl font-bold">
+          Rektor &apos;scopes
+        </h1>
+        <p className="text-base text-gray-400">
+          {new Date().toLocaleDateString("en-US", {
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+          })}
+          {sign && (
+            <span className="capitalize font-semibold text-gray-300">
+              : {sign}
+            </span>
+          )}
+        </p>
+      </div>
       <LocaleSelector />
-    </div>
+    </header>
   );
 };
 
